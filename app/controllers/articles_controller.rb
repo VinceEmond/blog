4883bced856fe67f50e758ea2040e3f3356e55e7
Vince Emond <vince.emond@icloud.com>
2022-04-06 12:26:20 -0700

    Initial Commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,56 @@
+class ArticlesController < ApplicationController
+  # index
+  def index
+    @articles = Article.all
+  end
+  
+  # show
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  # new
+  def new
+    @article = Article.new
+  end
+
+  # edit
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # create
+  def create
+    @article = Article.new(article_params)
+   
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  # update
+  def update
+    @article = Article.find(params[:id])
+ 
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  # destroy
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+   
+    redirect_to articles_path
+  end
+   
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+end
